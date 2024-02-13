2c670ee48e6b86e9351174428969d5e42031ed03
Bego <156913011+begumhere@users.noreply.github.com>
2024-02-13 11:57:29 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace ataletmakinesonn
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBoxM = new ComboBox();
+            comboBoxS = new ComboBox();
+            buttonH = new Button();
+            buttonS = new Button();
+            labela = new Label();
+            labelb = new Label();
+            labelh = new Label();
+            labelr = new Label();
+            labelMass = new Label();
+            labelVolume = new Label();
+            labelMassOut = new Label();
+            labelVolumeOut = new Label();
+            labelInertia = new Label();
+            labelInertiaOut = new Label();
+            textBoxa = new TextBox();
+            textBoxb = new TextBox();
+            textBoxh = new TextBox();
+            textBoxr = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // comboBoxM
+            // 
+            comboBoxM.FormattingEnabled = true;
+            comboBoxM.Items.AddRange(new object[] { "Alüminyum ", "Bakır", "Altın ", "Gümüş", "Çelik ", "Ahşap", "Kauçuk", "Plastik", "Titanyum", "Cam " });
+            comboBoxM.Location = new Point(34, 77);
+            comboBoxM.Name = "comboBoxM";
+            comboBoxM.Size = new Size(182, 33);
+            comboBoxM.TabIndex = 0;
+            // 
+            // comboBoxS
+            // 
+            comboBoxS.FormattingEnabled = true;
+            comboBoxS.Items.AddRange(new object[] { "Küp", "Dikdörtgen Prizma", "Küre", "Silindir", "Konik Prizma", "Piramit", "Paralelkenar Prizma", "Tetraedar", "Koni", "Dodekaedron" });
+            comboBoxS.Location = new Point(34, 170);
+            comboBoxS.Name = "comboBoxS";
+            comboBoxS.Size = new Size(182, 33);
+            comboBoxS.TabIndex = 1;
+            // 
+            // buttonH
+            // 
+            buttonH.Location = new Point(34, 254);
+            buttonH.Name = "buttonH";
+            buttonH.Size = new Size(182, 34);
+            buttonH.TabIndex = 2;
+            buttonH.Text = "Hesapla";
+            buttonH.UseVisualStyleBackColor = true;
+            buttonH.Click += buttonH_Click;
+            // 
+            // buttonS
+            // 
+            buttonS.Location = new Point(34, 321);
+            buttonS.Name = "buttonS";
+            buttonS.Size = new Size(182, 34);
+            buttonS.TabIndex = 3;
+            buttonS.Text = "Sil";
+            buttonS.UseVisualStyleBackColor = true;
+            buttonS.Click += buttonS_Click;
+            // 
+            // labela
+            // 
+            labela.AutoSize = true;
+            labela.Location = new Point(298, 75);
+            labela.Name = "labela";
+            labela.Size = new Size(134, 25);
+            labela.TabIndex = 4;
+            labela.Text = "Uzunluk (mm)=";
+            // 
+            // labelb
+            // 
+            labelb.AutoSize = true;
+            labelb.Location = new Point(289, 117);
+            labelb.Name = "labelb";
+            labelb.Size = new Size(136, 25);
+            labelb.TabIndex = 5;
+            labelb.Text = " Genişlik (mm)=";
+            // 
+            // labelh
+            // 
+            labelh.AutoSize = true;
+            labelh.Location = new Point(298, 173);
+            labelh.Name = "labelh";
+            labelh.Size = new Size(143, 25);
+            labelh.TabIndex = 6;
+            labelh.Text = "Yükseklik (mm)=";
+            // 
+            // labelr
+            // 
+            labelr.AutoSize = true;
+            labelr.Location = new Point(298, 220);
+            labelr.Name = "labelr";
+            labelr.Size = new Size(121, 25);
+            labelr.TabIndex = 7;
+            labelr.Text = "Yarıçap(mm)=";
+            // 
+            // labelMass
+            // 
+            labelMass.AutoSize = true;
+            labelMass.Location = new Point(321, 279);
+            labelMass.Name = "labelMass";
+            labelMass.Size = new Size(78, 25);
+            labelMass.TabIndex = 8;
+            labelMass.Text = "Kütle   =";
+            // 
+            // labelVolume
+            // 
+            labelVolume.AutoSize = true;
+            labelVolume.Location = new Point(321, 321);
+            labelVolume.Name = "labelVolume";
+            labelVolume.Size = new Size(79, 25);
+            labelVolume.TabIndex = 9;
+            labelVolume.Text = "Hacim =";
+            // 
+            // labelMassOut
+            // 
+            labelMassOut.AutoSize = true;
+            labelMassOut.Location = new Point(456, 280);
+            labelMassOut.Name = "labelMassOut";
+            labelMassOut.Size = new Size(0, 25);
+            labelMassOut.TabIndex = 10;
+            // 
+            // labelVolumeOut
+            // 
+            labelVolumeOut.AutoSize = true;
+            labelVolumeOut.Location = new Point(456, 321);
+            labelVolumeOut.Name = "labelVolumeOut";
+            labelVolumeOut.Size = new Size(0, 25);
+            labelVolumeOut.TabIndex = 11;
+            // 
+            // labelInertia
+            // 
+            labelInertia.AutoSize = true;
+            labelInertia.Location = new Point(321, 360);
+            labelInertia.Name = "labelInertia";
+            labelInertia.Size = new Size(78, 25);
+            labelInertia.TabIndex = 12;
+            labelInertia.Text = "Sonuç =";
+            // 
+            // labelInertiaOut
+            // 
+            labelInertiaOut.AutoSize = true;
+            labelInertiaOut.Location = new Point(455, 360);
+            labelInertiaOut.Name = "labelInertiaOut";
+            labelInertiaOut.Size = new Size(0, 25);
+            labelInertiaOut.TabIndex = 13;
+            // 
+            // textBoxa
+            // 
+            textBoxa.Location = new Point(455, 67);
+            textBoxa.Name = "textBoxa";
+            textBoxa.Size = new Size(150, 31);
+            textBoxa.TabIndex = 14;
+            // 
+            // textBoxb
+            // 
+            textBoxb.Location = new Point(455, 111);
+            textBoxb.Name = "textBoxb";
+            textBoxb.Size = new Size(150, 31);
+            textBoxb.TabIndex = 15;
+            // 
+            // textBoxh
+            // 
+            textBoxh.Location = new Point(455, 167);
+            textBoxh.Name = "textBoxh";
+            textBoxh.Size = new Size(150, 31);
+            textBoxh.TabIndex = 16;
+            // 
+            // textBoxr
+            // 
+            textBoxr.Location = new Point(455, 217);
+            textBoxr.Name = "textBoxr";
+            textBoxr.Size = new Size(150, 31);
+            textBoxr.TabIndex = 17;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 25);
+            label1.TabIndex = 18;
+            label1.Text = "Malzemeler";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(34, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 25);
+            label2.TabIndex = 19;
+            label2.Text = "Şekiller";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGray;
+            ClientSize = new Size(665, 422);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBoxr);
+            Controls.Add(textBoxh);
+            Controls.Add(textBoxb);
+            Controls.Add(textBoxa);
+            Controls.Add(labelInertiaOut);
+            Controls.Add(labelInertia);
+            Controls.Add(labelVolumeOut);
+            Controls.Add(labelMassOut);
+            Controls.Add(labelVolume);
+            Controls.Add(labelMass);
+            Controls.Add(labelr);
+            Controls.Add(labelh);
+            Controls.Add(labelb);
+            Controls.Add(labela);
+            Controls.Add(buttonS);
+            Controls.Add(buttonH);
+            Controls.Add(comboBoxS);
+            Controls.Add(comboBoxM);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBoxM;
+        private ComboBox comboBoxS;
+        private Button buttonH;
+        private Button buttonS;
+        private Label labela;
+        private Label labelb;
+        private Label labelh;
+        private Label labelr;
+        private Label labelMass;
+        private Label labelVolume;
+        private Label labelMassOut;
+        private Label labelVolumeOut;
+        private Label labelInertia;
+        private Label labelInertiaOut;
+        private TextBox textBoxa;
+        private TextBox textBoxb;
+        private TextBox textBoxh;
+        private TextBox textBoxr;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,437 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace ataletmakinesonn
+{
+    public partial class Form1 : Form
+    {
+        string tempParam = "";
+        public string Temporary
+        {
+            get { return tempParam; }
+        }
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            comboBoxS.SelectedIndexChanged += ComboBoxS_SelectedIndexChanged;
+
+        }
+
+        private void ComboBoxS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            var comboBoxValue2 = comboBoxS.GetItemText(comboBoxS.SelectedItem);
+            if (comboBoxValue2 != null && comboBoxValue2 == "Küp")
+            {
+                textBoxa.Visible = true;
+                labela.Visible = true;
+                textBoxb.Visible = false;
+                labelb.Visible = false;
+                textBoxh.Visible = false;
+                labelh.Visible = false;
+                textBoxr.Visible = false;
+                labelr.Visible = false;
+
+
+            }
+            if (comboBoxValue2 != null && comboBoxValue2 == "Dikdörtgen Prizma")
+            {
+                textBoxa.Visible = true;
+                labela.Visible = true;
+                textBoxb.Visible = true;
+                labelb.Visible = true;
+                textBoxh.Visible = true;
+                labelh.Visible = true;
+                textBoxr.Visible = false;
+                labelr.Visible = false;
+
+
+            }
+            if (comboBoxValue2 != null && comboBoxValue2 == "Küre")
+            {
+                textBoxa.Visible = false;
+                labela.Visible = false;
+                textBoxb.Visible = false;
+                labelb.Visible = false;
+                textBoxh.Visible = false;
+                labelh.Visible = false;
+                textBoxr.Visible = true;
+                labelr.Visible = true;
+
+
+            }
+            if (comboBoxValue2 != null && comboBoxValue2 == "Silindir")
+            {
+                textBoxa.Visible = false;
+                labela.Visible = false;
+                textBoxb.Visible = false;
+                labelb.Visible = false;
+                textBoxh.Visible = true;
+                labelh.Visible = true;
+                textBoxr.Visible = true;
+                labelr.Visible = true;
+
+
+            }
+            if (comboBoxValue2 != null && comboBoxValue2 == "Konik Prizma")
+            {
+                textBoxa.Visible = false;
+                labela.Visible = false;
+                textBoxb.Visible = false;
+                labelb.Visible = false;
+                textBoxh.Visible = true;
+                labelh.Visible = true;
+                textBoxr.Visible = true;
+                labelr.Visible = true;
+
+
+
+            }
+            if (comboBoxValue2 != null && comboBoxValue2 == "Piramit")
+            {
+                textBoxa.Visible = true;
+                labela.Visible = true;
+                textBoxb.Visible = true;
+                labelb.Visible = true;
+                textBoxh.Visible = true;
+                labelh.Visible = true;
+                textBoxr.Visible = false;
+                labelr.Visible = false;
+
+
+
+            }
+            if (comboBoxValue2 != null && comboBoxValue2 == "Paralelkenar Prizma")
+            {
+                textBoxa.Visible = true;
+                labela.Visible = true;
+                textBoxb.Visible = true;
+                labelb.Visible = true;
+                textBoxh.Visible = true;
+                labelh.Visible = true;
+                textBoxr.Visible = false;
+                labelr.Visible = false;
+
+
+
+            }
+            if (comboBoxValue2 != null && comboBoxValue2 == "Tetraedar")
+            {
+                textBoxa.Visible = true;
+                labela.Visible = true;
+                textBoxb.Visible = true;
+                labelb.Visible = true;
+                textBoxh.Visible = false;
+                labelh.Visible = false;
+                textBoxr.Visible = false;
+                labelr.Visible = false;
+
+
+            }
+            if (comboBoxValue2 != null && comboBoxValue2 == "Koni")
+            {
+                textBoxa.Visible = false;
+                labela.Visible = false;
+                textBoxb.Visible = false;
+                labelb.Visible = false;
+                textBoxh.Visible = true;
+                labelh.Visible = true;
+                textBoxr.Visible = true;
+                labelr.Visible = true;
+
+
+
+            }
+            if (comboBoxValue2 != null && comboBoxValue2 == "Dodekaedron")
+            {
+                textBoxa.Visible = true;
+                labela.Visible = true;
+                textBoxb.Visible = true;
+                labelb.Visible = true;
+                textBoxh.Visible = false;
+                labelh.Visible = false;
+                textBoxr.Visible = true;
+                labelr.Visible = true;
+
+
+            }
+        }
+
+        public void buttonH_Click(object sender, EventArgs e)
+        {
+
+            double V;
+            double Density;
+            double Mass;
+            double Inertia;
+            double a = 0;
+            double b = 0;
+            double h = 0;
+            double r = 0;
+
+
+
+            string texta = textBoxa.Text;
+            if (!string.IsNullOrWhiteSpace(texta) && !double.TryParse(texta, out a))
+            {
+                MessageBox.Show("Hatalı giriş yaptınız. Lütfen sayı giriniz.");
+            }
+
+
+            string textb = textBoxb.Text; 
+
+                if (!string.IsNullOrWhiteSpace(textb) && !double.TryParse(textb, out b))
+                {
+                    MessageBox.Show("Hatalı giriş yaptınız. Lütfen sayı giriniz.");
+
+                }
+
+            string textr = textBoxr.Text;
+            if (!string.IsNullOrWhiteSpace(textr) && !double.TryParse(textr, out r))
+            {
+                MessageBox.Show("Hatalı giriş yaptınız. Lütfen sayı giriniz.");
+            }
+
+
+            string texth = textBoxh.Text;
+            if (!string.IsNullOrWhiteSpace(texth) && !double.TryParse(texth, out h))
+            {
+                MessageBox.Show("Hatalı giriş yaptınız. Lütfen sayı giriniz.");
+                
+            }
+
+
+
+            var comboBoxValue1 = comboBoxM.GetItemText(comboBoxM.SelectedItem);
+
+            Density = 1;
+
+            if (comboBoxValue1 != null && comboBoxValue1 == "Çelik")
+            {
+                Density = 7.85;
+
+            }
+            else if (comboBoxValue1 != null && comboBoxValue1 == "Ahşap")
+            {
+                Density = 0.75;
+            }
+            else if (comboBoxValue1 != null && comboBoxValue1 == "Kauçuk")
+            {
+                Density = 1.20;
+            }
+            else if (comboBoxValue1 != null && comboBoxValue1 == "Plastik")
+            {
+                Density = 1.10;
+            }
+            else if (comboBoxValue1 != null && comboBoxValue1 == "Titanyum")
+            {
+                Density = 4.51;
+            }
+            else if (comboBoxValue1 != null && comboBoxValue1 == "Cam")
+            {
+                Density = 2.48;
+            }
+            else if (comboBoxValue1 != null && comboBoxValue1 == "Alüminyum")
+            {
+                Density = 2.7;
+            }
+            else if (comboBoxValue1 != null && comboBoxValue1 == "Bakır")
+            {
+                Density = 8.96;
+            }
+            else if (comboBoxValue1 != null && comboBoxValue1 == "Altın")
+            {
+                Density = 19.32;
+            }
+            else if (comboBoxValue1 != null && comboBoxValue1 == "Gümüş")
+            {
+                Density = 10.49;
+            }
+
+
+
+            var comboBoxValue2 = comboBoxS.GetItemText(comboBoxS.SelectedItem);
+
+
+            if (comboBoxValue2 != null && comboBoxValue2 == "Küp")
+            {
+
+
+
+
+
+                tempParam = "Küp";
+                V = Math.Pow(a, 3);
+                Mass = V * Density;
+                Inertia = (1.0 / 6.0) * Mass * a * a;
+
+                labelVolumeOut.Text = Convert.ToString(V);
+                labelMassOut.Text = Convert.ToString(Mass);
+                labelInertiaOut.Text = Convert.ToString(Inertia);
+
+
+
+
+            }
+            else
+            if (comboBoxValue2 != null && comboBoxValue2 == "Dikdörtgen Prizma")
+
+
+            {
+
+
+                tempParam = "Dikdörtgen Prizma";
+                V = a * b * h;
+                Mass = V * Density;
+                Inertia = (1.0 / 12.0) * Mass * (a * a + b * b);
+
+                labelVolumeOut.Text = Convert.ToString(V);
+                labelMassOut.Text = Convert.ToString(Mass);
+                labelInertiaOut.Text = Convert.ToString(Inertia);
+            }
+            else
+            if (comboBoxValue2 != null && comboBoxValue2 == "Küre")
+            {
+
+
+                tempParam = "Küre";
+
+                V = (4.0 / 3.0) * Math.PI * Math.Pow(r, 3);
+                Mass = V * Density;
+                Inertia = (2.0 / 5.0) * Mass * r * r;
+
+                labelVolumeOut.Text = Convert.ToString(V);
+                labelMassOut.Text = Convert.ToString(Mass);
+                labelInertiaOut.Text = Convert.ToString(Inertia);
+            }
+            else
+            if (comboBoxValue2 != null && comboBoxValue2 == "Silindir")
+            {
+
+
+                tempParam = "Silindir";
+                V = Math.PI * Math.Pow(r, 2) * h;
+                Mass = V * Density;
+                Inertia = (1.0 / 2.0) * Mass * r * r;
+
+                labelVolumeOut.Text = Convert.ToString(V);
+                labelMassOut.Text = Convert.ToString(Mass);
+                labelInertiaOut.Text = Convert.ToString(Inertia);
+
+            }
+            else if (comboBoxValue2 != null && comboBoxValue2 == "Konik Prizma")
+            {
+
+
+                tempParam = "Konik Prizma";
+                V = (1.0 / 3.0) * Math.PI * Math.Pow(r, 2) * h;
+                Mass = V * Density;
+                Inertia = (3.0 / 10.0) * Mass * r * r;
+
+                labelVolumeOut.Text = Convert.ToString(V);
+                labelMassOut.Text = Convert.ToString(Mass);
+                labelInertiaOut.Text = Convert.ToString(Inertia);
+            }
+            else if (comboBoxValue2 != null && comboBoxValue2 == "Piramit")
+            {
+
+
+
+
+                tempParam = "Piramit";
+                V = (1.0 / 3.0) * a * b * h;
+                Mass = V * Density;
+                Inertia = (1.0 / 3.0) * Mass * a * b * h;
+
+                labelVolumeOut.Text = Convert.ToString(V);
+                labelMassOut.Text = Convert.ToString(Mass);
+                labelInertiaOut.Text = Convert.ToString(Inertia);
+            }
+            else if (comboBoxValue2 != null && comboBoxValue2 == "Paralelkenar Prizma")
+            {
+
+
+                tempParam = "Paralelkenar Prizma";
+                V = a * b * h;
+                Mass = V * Density;
+                Inertia = (1 / 3) * Mass * (a * b) * h * (a * a + b * b) / 12;
+
+                labelVolumeOut.Text = Convert.ToString(V);
+                labelMassOut.Text = Convert.ToString(Mass);
+                labelInertiaOut.Text = Convert.ToString(Inertia);
+            }
+            else if (comboBoxValue2 != null && comboBoxValue2 == "Tetraedar")
+            {
+                tempParam = "Tetraedar";
+                V = (1.0 / 6.0) * Math.Sqrt(2) * Math.Pow(a, 3);
+                Mass = V * Density;
+                Inertia = Mass * ((b * b) / 12);
+
+                labelVolumeOut.Text = Convert.ToString(V);
+                labelMassOut.Text = Convert.ToString(Mass);
+                labelInertiaOut.Text = Convert.ToString(Inertia);
+            }
+            else if (comboBoxValue2 != null && comboBoxValue2 == "Koni")
+            {
+                tempParam = "Koni";
+                V = (1.0 / 3.0) * Math.PI * Math.Pow(r, 2) * h;
+                Mass = V * Density;
+                Inertia = (1.0 / 6.0) * Mass * Math.Pow(r, 2) * h;
+                labelVolumeOut.Text = Convert.ToString(V);
+                labelMassOut.Text = Convert.ToString(Mass);
+                labelInertiaOut.Text = Convert.ToString(Inertia);
+            }
+            else if (comboBoxValue2 != null && comboBoxValue2 == "Dodekaedron")
+            {
+                tempParam = "Dodekaedron";
+                V = (1.0 / 4.0) * (15 + 7 * Math.Sqrt(5)) * Math.Pow(a, 3);
+                Mass = V * Density;
+                Inertia = Mass * (Math.Pow(b, 2) + 15 + Math.Pow(r, 2)) / 12;
+                labelVolumeOut.Text = Convert.ToString(V);
+                labelMassOut.Text = Convert.ToString(Mass);
+                labelInertiaOut.Text = Convert.ToString(Inertia);
+            }
+
+
+
+
+
+
+        }
+
+        private void buttonS_Click(object sender, EventArgs e)
+        {
+            textBoxa.Clear();
+            textBoxb.Clear();
+            textBoxh.Clear();
+            textBoxr.Clear();
+            comboBoxM.SelectedIndex = -1;
+            comboBoxS.SelectedIndex = -1;
+
+            textBoxa.Visible = true;
+            labela.Visible = true;
+            textBoxb.Visible = true;
+            labelb.Visible = true;
+            textBoxh.Visible = true;
+            labelh.Visible = true;
+            textBoxr.Visible = true;
+            labelr.Visible = true;
+
+
+
+            labelVolumeOut.Text = "";
+            labelMassOut.Text = "";
+            labelInertiaOut.Text = "";
+
+            tempParam = "";
+
+        }
+
+      
+    }
+}
